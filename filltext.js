0218ab69d1f7b6230e781708622c07311f9fa4db
philippe MEYER <pmg.meyer@gmail.com>
2019-07-29 22:06:34 +0200

    first commit work in progress

---
+++ filltext.js
@@ -0,0 +1,503 @@
+
+let filltextMockData = [
+    {
+        "fname": "Jaime",
+        "lname": "Lehman",
+        "company": "Abrahams Technologies",
+        "city": "Derby",
+        "zip": 72917,
+        "country": "Tuvalu",
+        "phone": "8571491333",
+        "active": true
+    },
+    {
+        "fname": "Valoria",
+        "lname": "Gibbs",
+        "company": "Weakliem Marketing",
+        "city": "Temecula",
+        "zip": 59835,
+        "country": "Guadeloupe",
+        "phone": "3159986746",
+        "active": true
+    },
+    {
+        "fname": "Jeremiah",
+        "lname": "Kim",
+        "company": "Nordby Global",
+        "city": "Spencer",
+        "zip": 29633,
+        "country": "Slovakia",
+        "phone": "4329171534",
+        "active": true
+    },
+    {
+        "fname": "Luciana",
+        "lname": "Walls",
+        "company": "Teixeira Industries",
+        "city": "Longmont",
+        "zip": 38451,
+        "country": "Kyrgyzstan",
+        "phone": "5450598756",
+        "active": false
+    },
+    {
+        "fname": "Debra",
+        "lname": "Cardenas",
+        "company": "Reinhardt Brothers",
+        "city": "Burke",
+        "zip": 62749,
+        "country": "Tanzania, United Republic of",
+        "phone": "7562157745",
+        "active": false
+    },
+    {
+        "fname": "Cheryll",
+        "lname": "Serraon",
+        "company": "Mccollum Technologies",
+        "city": "Central Valley",
+        "zip": 20682,
+        "country": "Turkey",
+        "phone": "9878520277",
+        "active": true
+    },
+    {
+        "fname": "Quincy",
+        "lname": "Jeffery",
+        "company": "Siegel Services",
+        "city": "White Bear Lake",
+        "zip": 66474,
+        "country": "Micronesia (Federated States of)",
+        "phone": "6526856514",
+        "active": true
+    },
+    {
+        "fname": "Stan",
+        "lname": "Spiegel",
+        "company": "Sjaardema Incorporated",
+        "city": "Dallas",
+        "zip": 39417,
+        "country": "Guyana",
+        "phone": "7322543416",
+        "active": false
+    },
+    {
+        "fname": "Devon",
+        "lname": "Jones",
+        "company": "Hancock Services",
+        "city": "St Augustine",
+        "zip": 45012,
+        "country": "Saint Vincent and the Grenadines",
+        "phone": "8204253072",
+        "active": true
+    },
+    {
+        "fname": "Wendy",
+        "lname": "Mcwethy",
+        "company": "Ellington Brothers",
+        "city": "Lees Summit",
+        "zip": 13205,
+        "country": "Åland Islands",
+        "phone": "6283514418",
+        "active": false
+    },
+    {
+        "fname": "Rahul",
+        "lname": "Pick",
+        "company": "Correll Brothers",
+        "city": "Swansea",
+        "zip": 26579,
+        "country": "Thailand",
+        "phone": "3323367470",
+        "active": true
+    },
+    {
+        "fname": "Ted",
+        "lname": "Packard",
+        "company": "Castellanos Industries",
+        "city": "Saint Ann",
+        "zip": 61329,
+        "country": "Armenia",
+        "phone": "9439884450",
+        "active": true
+    },
+    {
+        "fname": "Rosa",
+        "lname": "Speck",
+        "company": "Marrinson Corporation",
+        "city": "Sinton",
+        "zip": 17097,
+        "country": "Guam",
+        "phone": "6269495120",
+        "active": false
+    },
+    {
+        "fname": "Kay",
+        "lname": "Coleman",
+        "company": "Nimick International",
+        "city": "Saint Ann",
+        "zip": 44479,
+        "country": "Denmark",
+        "phone": "6638024551",
+        "active": true
+    },
+    {
+        "fname": "Tijana",
+        "lname": "Vinsant",
+        "company": "Day Shipping",
+        "city": "Brunswick",
+        "zip": 20675,
+        "country": "Christmas Island",
+        "phone": "1330017014",
+        "active": true
+    },
+    {
+        "fname": "Ted",
+        "lname": "Ramirez",
+        "company": "Blayne Corporation",
+        "city": "Cedartown",
+        "zip": 19930,
+        "country": "Ghana",
+        "phone": "2229153225",
+        "active": false
+    },
+    {
+        "fname": "Liang",
+        "lname": "Contreras",
+        "company": "Wolz Incorporated",
+        "city": "Miami",
+        "zip": 29899,
+        "country": "Norfolk Island",
+        "phone": "1211920882",
+        "active": false
+    },
+    {
+        "fname": "Renardo",
+        "lname": "More",
+        "company": "Schotuen Foods",
+        "city": "San Gabriel Valley",
+        "zip": 65579,
+        "country": "Jersey",
+        "phone": "5908547291",
+        "active": false
+    },
+    {
+        "fname": "Virginia",
+        "lname": "Eugene",
+        "company": "Pillai LLC",
+        "city": "Alton",
+        "zip": 62389,
+        "country": "Gibraltar",
+        "phone": "9066524367",
+        "active": false
+    },
+    {
+        "fname": "Dante",
+        "lname": "Graham",
+        "company": "Dehaven Services",
+        "city": "Detroit",
+        "zip": 43622,
+        "country": "Oman",
+        "phone": "3493461671",
+        "active": false
+    },
+    {
+        "fname": "Ratikorn",
+        "lname": "Willett",
+        "company": "Mcnamee Corporation",
+        "city": "Lafayette",
+        "zip": 31704,
+        "country": "Tunisia",
+        "phone": "1321176172",
+        "active": false
+    },
+    {
+        "fname": "Rick",
+        "lname": "Mashburn",
+        "company": "Zuniga International",
+        "city": "Bremen",
+        "zip": 92187,
+        "country": "Latvia",
+        "phone": "6426570175",
+        "active": true
+    },
+    {
+        "fname": "Ishtiaq",
+        "lname": "Buckaloo",
+        "company": "Aubut International",
+        "city": "Simi Valley",
+        "zip": 84388,
+        "country": "Tunisia",
+        "phone": "1831845254",
+        "active": false
+    },
+    {
+        "fname": "Jasper",
+        "lname": "Bubel",
+        "company": "Bouchard Incorporated",
+        "city": "North Bend",
+        "zip": 42726,
+        "country": "Ukraine",
+        "phone": "2720819061",
+        "active": true
+    },
+    {
+        "fname": "Narendra",
+        "lname": "Mcglaughlin",
+        "company": "Taverna Industries",
+        "city": "Bellefontaine",
+        "zip": 52783,
+        "country": "Falkland Islands (Malvinas)",
+        "phone": "7149198186",
+        "active": false
+    },
+    {
+        "fname": "Abigail",
+        "lname": "Demerath",
+        "company": "Schade Brothers",
+        "city": "Fort Campbell",
+        "zip": 21158,
+        "country": "San Marino",
+        "phone": "1172983292",
+        "active": true
+    },
+    {
+        "fname": "Aydin",
+        "lname": "Terrill",
+        "company": "Yarber Corporation",
+        "city": "Wahiawa",
+        "zip": 59273,
+        "country": "Pakistan",
+        "phone": "4172546009",
+        "active": false
+    },
+    {
+        "fname": "Noopur",
+        "lname": "Ritchie",
+        "company": "Geis Foods",
+        "city": "Lancaster",
+        "zip": 67546,
+        "country": "Latvia",
+        "phone": "2976618099",
+        "active": false
+    },
+    {
+        "fname": "Rachmat",
+        "lname": "Schaus",
+        "company": "Ast Foods",
+        "city": "Owensboro",
+        "zip": 60458,
+        "country": "Yemen",
+        "phone": "5270510141",
+        "active": false
+    },
+    {
+        "fname": "Aubrey",
+        "lname": "Conn",
+        "company": "Mclaurin Computing",
+        "city": "Panama City Beach",
+        "zip": 24273,
+        "country": "Myanmar",
+        "phone": "9454878847",
+        "active": false
+    },
+    {
+        "fname": "Pele",
+        "lname": "Cahill",
+        "company": "Pennell Brothers",
+        "city": "Rockville",
+        "zip": 83725,
+        "country": "Oman",
+        "phone": "5093570641",
+        "active": false
+    },
+    {
+        "fname": "Imelda",
+        "lname": "Oliva",
+        "company": "Borgford Foods",
+        "city": "Saltilo",
+        "zip": 88523,
+        "country": "Timor-Leste",
+        "phone": "4411804465",
+        "active": false
+    },
+    {
+        "fname": "Romeo",
+        "lname": "Snyder",
+        "company": "Wood Incorporated",
+        "city": "Texas City",
+        "zip": 67878,
+        "country": "Honduras",
+        "phone": "3466144752",
+        "active": true
+    },
+    {
+        "fname": "Takita",
+        "lname": "Vaimauga",
+        "company": "Spencer Technologies",
+        "city": "Modesto",
+        "zip": 80473,
+        "country": "Andorra",
+        "phone": "7328582559",
+        "active": false
+    },
+    {
+        "fname": "Maya",
+        "lname": "Kaur",
+        "company": "Bishop Shipping",
+        "city": "Tampa",
+        "zip": 41183,
+        "country": "Slovakia",
+        "phone": "6124756373",
+        "active": true
+    },
+    {
+        "fname": "Corazon",
+        "lname": "Primavera",
+        "company": "Ortega Technologies",
+        "city": "Pacific Palisades",
+        "zip": 97530,
+        "country": "Denmark",
+        "phone": "3704182694",
+        "active": true
+    },
+    {
+        "fname": "Nate",
+        "lname": "Radke",
+        "company": "Polovich Industries",
+        "city": "Faribault",
+        "zip": 98039,
+        "country": "Iran (Islamic Republic of)",
+        "phone": "2415583338",
+        "active": true
+    },
+    {
+        "fname": "Marla",
+        "lname": "Medeiros",
+        "company": "Band Incorporated",
+        "city": "York",
+        "zip": 62614,
+        "country": "Bouvet Island",
+        "phone": "6900196411",
+        "active": true
+    },
+    {
+        "fname": "Brendan",
+        "lname": "Rush",
+        "company": "Deskins Brothers",
+        "city": "Andover",
+        "zip": 80592,
+        "country": "Western Sahara",
+        "phone": "8060013810",
+        "active": false
+    },
+    {
+        "fname": "Eric",
+        "lname": "Gould",
+        "company": "Hudson Shipping",
+        "city": "Edwards",
+        "zip": 44133,
+        "country": "Seychelles",
+        "phone": "4362614577",
+        "active": true
+    },
+    {
+        "fname": "Jose",
+        "lname": "Proia",
+        "company": "Taverna Corporation",
+        "city": "York",
+        "zip": 57958,
+        "country": "Monaco",
+        "phone": "3189632781",
+        "active": true
+    },
+    {
+        "fname": "LaFaye",
+        "lname": "Betts",
+        "company": "Breuninger Computing",
+        "city": "Palm Harbor",
+        "zip": 89293,
+        "country": "Somalia",
+        "phone": "1814380912",
+        "active": true
+    },
+    {
+        "fname": "Earl",
+        "lname": "Hellman",
+        "company": "Bunch International",
+        "city": "Calgary",
+        "zip": 88006,
+        "country": "Guinea-Bissau",
+        "phone": "6448856820",
+        "active": true
+    },
+    {
+        "fname": "Don",
+        "lname": "Hardy",
+        "company": "Beonde Corporation",
+        "city": "Chambersburg",
+        "zip": 95321,
+        "country": "Faroe Islands",
+        "phone": "1644274892",
+        "active": false
+    },
+    {
+        "fname": "Terrance",
+        "lname": "Hess",
+        "company": "Lopez Technologies",
+        "city": "Saint Clair Shores",
+        "zip": 47333,
+        "country": "Rwanda",
+        "phone": "6372172984",
+        "active": true
+    },
+    {
+        "fname": "Kameko",
+        "lname": "Mcmyne",
+        "company": "Mcalevey Global",
+        "city": "San Gabriel Valley",
+        "zip": 45456,
+        "country": "Mongolia",
+        "phone": "4866903304",
+        "active": false
+    },
+    {
+        "fname": "Staci",
+        "lname": "Palmer",
+        "company": "Tarbox Computing",
+        "city": "Lees Summit",
+        "zip": 97346,
+        "country": "Greece",
+        "phone": "2850388765",
+        "active": true
+    },
+    {
+        "fname": "Sara",
+        "lname": "Salter",
+        "company": "Reimer Technologies",
+        "city": "Derby",
+        "zip": 88270,
+        "country": "Portugal",
+        "phone": "1354417821",
+        "active": false
+    },
+    {
+        "fname": "Francesca",
+        "lname": "Sutton",
+        "company": "Gonzalez International",
+        "city": "St. Louis",
+        "zip": 92317,
+        "country": "Ecuador",
+        "phone": "9248315857",
+        "active": true
+    },
+    {
+        "fname": "Jeremiah",
+        "lname": "Bowers",
+        "company": "Deals LLC",
+        "city": "Hampstead",
+        "zip": 80853,
+        "country": "Cabo Verde",
+        "phone": "4511141451",
+        "active": false
+    }
+]
